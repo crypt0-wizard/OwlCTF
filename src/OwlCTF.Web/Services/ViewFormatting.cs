97c22656f03b1d589f16ed9c42955f9d7d5201ab
Xor-ED <hoorulain276@gmail.com>
2026-08-31 16:34:48 +0500

    Minor fixes

--- src/OwlCTF.Web/Services/ViewFormatting.cs
+++ src/OwlCTF.Web/Services/ViewFormatting.cs
@@ -19,3 +19,26 @@ public static class TimeDisplay
     public static string UtcIso(DateTime value) =>
         DateTime.SpecifyKind(value, DateTimeKind.Utc).ToString("O");
 }
+
+public static class FileSizeDisplay
+{
+    private static readonly string[] Units = ["B", "KiB", "MiB", "GiB"];
+
+    public static string Format(long bytes)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(bytes);
+
+        var value = (double)bytes;
+        var unit = 0;
+        while (value >= 1024 && unit < Units.Length - 1)
+        {
+            value /= 1024;
+            unit++;
+        }
+
+        var number = unit == 0
+            ? bytes.ToString(System.Globalization.CultureInfo.InvariantCulture)
+            : value.ToString("0.#", System.Globalization.CultureInfo.InvariantCulture);
+        return $"{number} {Units[unit]}";
+    }
+}
